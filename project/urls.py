f2559c70b1d09dc03f46715a2066abf2bbcea853
lexsos <ale-sosnin@yandex.ru>
2013-12-19 11:40:11 +0400

    Add index

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
@@ -17,6 +18,11 @@ urlpatterns = patterns('',
     url(r'^documents/', include('documents.urls')),
     url(r'^avia_park/', include('avia_park.urls')),
     url(r'^news/', include('news.urls')),
+
+    url(r'^$',
+        TemplateView.as_view(template_name='index.html'),
+        name='index',
+    ),
 )
 
 if settings.DEBUG:
