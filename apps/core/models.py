828d579df010c04849dcb12e14b54d4cfdb5f828
lexsos <ale-sosnin@mail.ru>
2018-03-09 20:34:01 +0300

    add russification for core app

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 class PageContent(models.Model):
@@ -8,20 +7,20 @@ class PageContent(models.Model):
         'main_page', 'schedule_list', 'services_list', 'news_list', 'news_detail', 'document_list', 'contact_list',
         'faq_list', 'job_list')
     PAGE_CHOICES = (
-        (MAIN, _('main page')), (SCHEDULE, _('schedule page')), (SERVICES, _('services page')),
-        (NEWS_LIST, _('news list page')), (NEWS_DETAIL, _('news detail page')), (DOCUMENTS, _('documents page')),
-        (CONTACTS, _('contacts page')), (FAQ, _('faq page')), (JOB, _('job page')))
+        (MAIN, 'главна'), (SCHEDULE, 'расписание'), (SERVICES, 'услуги'), (NEWS_LIST, 'список новостей'),
+        (NEWS_DETAIL, 'страница новости'), (DOCUMENTS, 'документы'), (CONTACTS, 'контакты'), (FAQ, 'faq'),
+        (JOB, 'вакансии'))
 
-    page = models.CharField(verbose_name=_('page'), choices=PAGE_CHOICES, default=MAIN, max_length=50, unique=True)
-    additional_header = models.TextField(verbose_name=_('additional header'), blank=True)
-    additional_footer = models.TextField(verbose_name=_('additional footer'), blank=True)
-    slider_proxy = models.TextField(verbose_name=_('slider proxy'), blank=True)
-    enabled = models.BooleanField(verbose_name=_('enabled'), default=True, help_text=_('show/hide record.'))
+    page = models.CharField(verbose_name='страница', choices=PAGE_CHOICES, default=MAIN, max_length=50, unique=True)
+    additional_header = models.TextField(verbose_name='дополнительный заголовк', blank=True)
+    additional_footer = models.TextField(verbose_name='дополнительный подвал', blank=True)
+    slider_proxy = models.TextField(verbose_name='слайдер', blank=True)
+    enabled = models.BooleanField(verbose_name='включено', default=True)
 
     class Meta:
         ordering = ['page']
-        verbose_name_plural = _('pages content')
-        verbose_name = _('page content')
+        verbose_name_plural = 'содержимое страниц'
+        verbose_name = 'содержимое страницы'
 
     def __str__(self):
         return self.page
